659439877db0ee05d0a3427fcf231376a3642a5b
Andr� Silveira <andresilveira_3101@hotmail.com>
2019-06-04 20:21:50 -0300

    vamos la

--- src/applicassandradb/AppliCassandraDB.java
+++ src/applicassandradb/AppliCassandraDB.java
@@ -1,9 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package applicassandradb;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
 
 /**
  *
@@ -16,6 +16,27 @@ public class AppliCassandraDB {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+        Cluster cluster;
+        Session session;
+        cluster = Cluster.builder().addContactPoint("localhost").build();
+        session = cluster.connect("teste");
+        //session.execute("insert");
+        String catid = null, pdtid = null, pdtname = null, sa = null;
+        float price = 0;
+        ResultSet results = session.execute("select * from products");
+        for (Row row : results) {
+            pdtid = Integer.toString(row.getInt("pdt_id"));
+            catid = Integer.toString(row.getInt("cat_id"));
+            pdtname = row.getString("pdt_name");
+            price = row.getFloat("price");
+            sa = row.getString("shippingaddress");
+            
+            System.out.println("PDTid: " + pdtid);
+            System.out.println("Name" + pdtname);
+            System.out.println("CatID" + catid);
+            System.out.println("Preco: " + price);
+            System.out.println("SA: " + sa);
+        }
+        cluster.close();
+  }
 }
